fbf5284390688333c30eef928b2216531110c10d
Allen Barker <Allen.L.Barker@gmail.com>
2017-10-24 18:45:23 -0400

    clean up some unneeded files

--- doc/sphinx_docs/reset_pygments_style.bash
+++ doc/sphinx_docs/reset_pygments_style.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp $py/pygments*/alb.py /home/alb/programming/python/typped/VENV_py3/lib/python3.5/site-packages/pygments/styles
+cp $py/pygments*/alb.py $py/typped/VENV_py3/lib/python3.5/site-packages/pygments/styles
 
